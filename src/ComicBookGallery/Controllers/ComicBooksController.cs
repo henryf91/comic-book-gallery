5323c683345bee4836c419ee1a61606f3511886e
henry <henryf.gomez@gmail.com>
2018-01-17 16:33:19 -0600

    Detail View Strongly Typed

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ComicBookGallery.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,19 +11,23 @@ namespace ComicBookGallery.Controllers
     {
         public ActionResult Detail()
         {
+            var comicBook = new ComicBook()
+            {
+                SeriesTitle = "Up",
+                IssueNumber = 700,
+                DescriptionHtml = "<p> Final issue!Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
+                Artists = new Artist[]
+                {
+                    new Artist { Name = "Dan Slott", Role = "Script" },
+                    new Artist { Name = "Humberto Ramos", Role = "Pencils" },
+                    new Artist { Name = "Victor Olazaba", Role = "Inks" },
+                    new Artist { Name = "Edgar Delgado", Role = "Colors" },
+                    new Artist { Name = "Chris Eliopoulos", Role = "Letters" }
+                }
 
-            ViewBag.SeriesTittle = "Up";
-            ViewBag.IssueNumber = 700;
-            ViewBag.Description = "<p> Final issue!Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>";
-            ViewBag.Artists = new string[] {
-                "Script: Dan Slott",
-                "Pencils: Humberto Ramos",
-                "Inks: Victor Olazaba",
-                "Colors: Edgar Delgado",
-                "Letters: Chris Eliopoulos"
-            };
-
-            return View();
+        };
+          
+            return View(comicBook);
         }
     }
 }
