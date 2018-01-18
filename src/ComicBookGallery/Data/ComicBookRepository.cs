7e3e689dfc29b3ecd9db4ab5efd8a027aeb20d9f
henry <henryf.gomez@gmail.com>
2018-01-18 15:41:01 -0600

    Added view in ComicBook

--- src/ComicBookGallery/Data/ComicBookRepository.cs
+++ src/ComicBookGallery/Data/ComicBookRepository.cs
@@ -60,6 +60,11 @@ namespace ComicBookGallery.Data
             },
         };
 
+        public ComicBook[] GetComicBooks()
+        {
+            return _comicBooks;
+        }
+
         public ComicBook GetComicBook(int Id)
         {
             ComicBook comicBookToReturn = null;
